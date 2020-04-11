dda62247833834cf7b25f6596f28abe16bc0fc8c
charon1212 <CharonS2turquoise@gmail.com>
2020-04-11 09:41:12 +0900

    dev #1 add:単体テストプロジェクトを追加

---
+++ MyLibraryForCompProgramming/UnitTest/SampleUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SampleUnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
