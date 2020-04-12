66dea2c1349be7770deac3c33cf70bdcdc8ba840
charon1212 <CharonS2turquoise@gmail.com>
2020-04-12 14:31:30 +0900

    dev #6 add:素数に関する処理を追加

---
+++ MyLibraryForCompProgramming/MyLibraryForCompProgramming/Prime.cpp
@@ -0,0 +1,42 @@
+#include<map>
+
+// ëfêîîªíË
+bool is_prime(long long x) {
+    for (long long i = 2; i * i <= x; i++) if (x % i == 0) return false;
+    return true;
+}
+
+// ëfàˆêîï™â
+std::map<long long, int> primeFactor(long long x) {
+    std::map<long long, int> ret;
+    // 2,3ÇêÊÇ…Ç‚ÇÈÅB
+    while (x % 2 == 0) {
+        ret[2]++;
+        x /= 2;
+    }
+    while (x % 3 == 0) {
+        ret[3]++;
+        x /= 3;
+    }
+    bool sw = true;
+    long long factor = 5ll;
+    while (factor * factor <= x) {
+        while (x % factor == 0) {
+            ret[factor]++;
+            x /= factor;
+        }
+        // 6n+1Ç∆6n+5ÇÃÇ›ÇëŒè€Ç∆Ç∑ÇÈÇΩÇﬂÇÃçXêVéÆ
+        factor += sw ? 2 : 4;
+        sw != sw;
+    }
+    if (x != 1) ret[x] = 1;
+    return ret;
+}
+
+// ñÒêîÇÃëçêîÇéÊìæ
+int divisorCount(long long x) {
+    auto pFactor = primeFactor(x);
+    int ret = 1;
+    for (auto it = pFactor.begin(); it != pFactor.end(); it++) { ret *= (it->second + 1); }
+    return ret;
+}
