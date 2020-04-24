a1d1dfa5d407600d70b8e0c6033481cd6271e23f
charon1212 <CharonS2turquoise@gmail.com>
2020-04-24 21:58:01 +0900

    dev #7 fix:メソッド引数の名前をわかりやすく修正

--- MyLibraryForCompProgramming/MyLibraryForCompProgramming/VectorUtil.cpp
+++ MyLibraryForCompProgramming/MyLibraryForCompProgramming/VectorUtil.cpp
@@ -1,24 +1,24 @@
 #include <vector>
 #include <algorithm>
 
-template<typename T> inline std::vector<std::vector<T>> construct_vector_2d(int n, int m, T initialValue) {
-    auto r = std::vector<std::vector<T>>(n);
-    for (int i = 0; i < m; i++) r[i] = std::vector<T>(m, initialValue);
+template<typename T> inline std::vector<std::vector<T>> construct_vector_2d(int arraySize1, int arraySize2, T initialValue) {
+    auto r = std::vector<std::vector<T>>(arraySize1);
+    for (int i = 0; i < arraySize1; i++) r[i] = std::vector<T>(arraySize2, initialValue);
     return r;
 }
 
-template<typename T> inline std::vector<std::vector<std::vector<T>>> construct_vector_3d(int n, int m, int l, T initialValue) {
-    auto r = std::vector<std::vector<std::vector<T>>>(n);
-    for (int i = 0; i < n; i++) {
-        r[i] = std::vector<std::vector<T>>(m);
-        for (int j = 0; j < m; j++) r[i][j] = std::vector<T>(l, initialValue);
+template<typename T> inline std::vector<std::vector<std::vector<T>>> construct_vector_3d(int arraySize1, int arraySize2, int arraySize3, T initialValue) {
+    auto r = std::vector<std::vector<std::vector<T>>>(arraySize1);
+    for (int i = 0; i < arraySize1; i++) {
+        r[i] = std::vector<std::vector<T>>(arraySize2);
+        for (int j = 0; j < arraySize2; j++) r[i][j] = std::vector<T>(arraySize3, initialValue);
     }
     return r;
 }
 
-template<typename T> inline typename std::vector<T>::iterator find_binary_search(typename std::vector<T>::iterator begin, typename std::vector<T>::iterator end, T value) {
+template<typename T> inline typename std::vector<T>::iterator find_binary_search(typename std::vector<T>::iterator first, typename std::vector<T>::iterator last, T value) {
 
-    auto it = std::lower_bound(begin, end, value);
+    auto it = std::lower_bound(first, last, value);
     if ((*it) == value) return it;
     return end;
 
