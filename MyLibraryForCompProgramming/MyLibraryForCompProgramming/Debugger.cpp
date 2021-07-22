349280aea6f3fb85ec2a207facf31b4b99ccf48c
charon1212 <charons2turquoise@gmail.com>
2021-07-22 17:48:39 +0900

    デバッグ用関数を追加

---
+++ MyLibraryForCompProgramming/MyLibraryForCompProgramming/Debugger.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+template<typename T> void debugVec(std::vector <T> v) {
+    for (auto it = v.begin(); it != v.end(); it++) {
+        std::cout << (*it) << ",";
+    }
+    std::cout << endl;
+}
+
+template<typename T> void debugVec2(std::vector < std::vector<T>> v) {
+    for (auto it1 = v.begin(); it1 != v.end(); it1++) {
+        for (auto it2 = (*it1).begin(); it2 != (*it1).end(); it2++) {
+            std::cout << (*it2) << ",";
+        }
+        std::cout << endl;
+    }
+}
